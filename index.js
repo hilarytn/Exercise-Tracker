8d8cc864d430e4151ebe64435d636d507b4356d6
Hilary Titus Naor <hilarytn@gmail.com>
2023-12-04 13:04:45 +0100

    Add packages

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+const app = express()
+
+dotenv.config()
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+
+
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
